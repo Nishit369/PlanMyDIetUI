fb0b4fc8f50d49c010c77fe609ee4a3e934066b9
Nishit Barbhaya <nishitbarbhaya06@gmail.com>
2025-01-26 13:11:51 +0530

    page font

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -4,18 +4,19 @@ import gsap from 'gsap';
 import { Mesh } from 'three';
 
 export const AnimatedText: React.FC = () => {
-  // Define types for refs
+  
   const textRef2 = useRef<Mesh | null>(null);
   const textRef3 = useRef<Mesh | null>(null);
   const textRef4 = useRef<Mesh | null>(null);
   const textRefs = useRef<(Mesh | null)[]>([]);
 
   useEffect(() => {
-    // Animate each text in the array
+  
     textRefs.current.forEach((text, i) => {
       if (text) {
         gsap.to(text.position, {
-          y: 1.2,
+          y: 1.1,
+          x: text.position.x -=0.25,
           duration: 1,
           delay: i * 0.2,
           ease: "power2.inOut",
@@ -23,10 +24,10 @@ export const AnimatedText: React.FC = () => {
       }
     });
 
-    // Animate individual refs
+  
     if (textRef2.current) {
       gsap.to(textRef2.current.position, {
-        x: 1.4,
+        x: 1.2,
         y: 0.5,
         duration: 1.5,
         delay: 2,
@@ -35,7 +36,7 @@ export const AnimatedText: React.FC = () => {
 
     if (textRef3.current) {
       gsap.to(textRef3.current.position, {
-        x: 1.17,
+        x: 0.97,
         y: 0.1,
         duration: 1.5,
         delay: 3,
@@ -44,7 +45,7 @@ export const AnimatedText: React.FC = () => {
 
     if (textRef4.current) {
       gsap.to(textRef4.current.position, {
-        x: 1.1,
+        x: 0.9,
         y: -0.3,
         duration: 1.5,
         delay: 4,
