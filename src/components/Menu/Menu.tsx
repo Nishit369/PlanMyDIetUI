d3bf8e9a26c7d7be5c34fbd0f4ac3c2b92dd7123
Nishit Barbhaya <nishitbarbhaya06@gmail.com>
2025-01-27 17:34:50 +0530

    loading page addons

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -43,7 +43,7 @@ const Menu: React.FC<MenuProps> = ({ isOpen, onChange, menuOpen, menuChange }) =
             onClick={handleClickSound}
             onMouseOver={handleHoverSound}
           >
-            <span className="material-icons" style={{ fontSize: '1.9rem' }}>
+            <span className="material-icons" style={{ fontSize: '2vw' }}>
               {item.icon}
             </span>
             {item.itemText}
